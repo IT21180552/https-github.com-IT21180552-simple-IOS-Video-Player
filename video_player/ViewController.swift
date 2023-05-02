0646ef1417188397a9465b92fdb73195bd0668d4
IT21180552 <it21180552@my.sliit.lk>
2023-05-03 00:25:23 +0530

    Initial Commit

---
+++ video_player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Bathiya Pathum on 2023-05-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
